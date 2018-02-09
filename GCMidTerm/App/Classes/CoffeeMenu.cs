e2191197bdab05150be94117311d2ed280b1e0f0
Jakyle <jjackson360@gmail.com>
2018-02-09 12:24:20 -0500

    added CoffeeMenu.cs

---
+++ GCMidTerm/App/Classes/CoffeeMenu.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace MidtermProject.Classes
+{
+    class CoffeeMenu
+    {
+        public List<CoffeeObj> Items { get; set; }
+
+        public CoffeeMenu()
+        {
+            StreamReader read = new StreamReader("../../App/Classes/TextFile1.txt");
+
+            string currentLine = read.ReadLine();
+            List<string> CoffeeName = new List<string>();
+            while (currentLine != null)
+            {
+                CoffeeName.Add(currentLine);
+                currentLine = read.ReadLine();
+            }
+            read.Close();
+
+            Items = new List<CoffeeObj>
+            {
+                new CoffeeObj()
+            }
+        }
+    }
+}
