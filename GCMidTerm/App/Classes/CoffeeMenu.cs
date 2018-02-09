be9a0a09533f9dd25933aadc98b904afc70f104b
Jakyle <jjackson360@gmail.com>
2018-02-09 12:39:39 -0500

    fleshed out coffeemenu class

--- GCMidTerm/App/Classes/CoffeeMenu.cs
+++ GCMidTerm/App/Classes/CoffeeMenu.cs
@@ -9,6 +9,7 @@ namespace MidtermProject.Classes
 
         public CoffeeMenu()
         {
+            #region pulls coffee names from .txt
             StreamReader read = new StreamReader("../../App/Classes/TextFile1.txt");
 
             string currentLine = read.ReadLine();
@@ -19,11 +20,24 @@ namespace MidtermProject.Classes
                 currentLine = read.ReadLine();
             }
             read.Close();
-
+            #endregion
+            #region saves list of coffee with name and price
             Items = new List<CoffeeObj>
             {
-                new CoffeeObj()
-            }
+                new CoffeeObj(CoffeeName[0], 1.50D),
+                new CoffeeObj(CoffeeName[1], .99D),
+                new CoffeeObj(CoffeeName[2], 3.50D),
+                new CoffeeObj(CoffeeName[3], 4.00D),
+                new CoffeeObj(CoffeeName[4], 4.25D),
+                new CoffeeObj(CoffeeName[5], 3.50D),
+                new CoffeeObj(CoffeeName[6], 3.75D),
+                new CoffeeObj(CoffeeName[7], 4.00D),
+                new CoffeeObj(CoffeeName[8], 2.00D),
+                new CoffeeObj(CoffeeName[9], 3.50D),
+                new CoffeeObj(CoffeeName[10], 3.00D),
+                new CoffeeObj(CoffeeName[11], 4.00D),
+            };
+            #endregion
         }
     }
 }
