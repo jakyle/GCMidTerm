e2137a17f4c8c9714d38c59092fbdc2d96fa7c54
Audrae Christy <audi037@yahoo.com>
2018-02-10 14:30:38 -0500

    register cash/cc/check

--- GCMidTerm/App/Classes/Register.cs
+++ GCMidTerm/App/Classes/Register.cs
@@ -84,32 +84,35 @@ namespace MidtermProject.Classes
             Console.WriteLine("Enter total payment due: ");
             double cashAmount = double.Parse(Console.ReadLine()); //setting variable for user's cash amount
             PmtCash cash = new PmtCash(cashAmount); //making new cash object with cash class
-
         }
-
         private static void PaymentCheck()
         {
             Console.WriteLine("Enter total payment due: ");
             double checkAmount = double.Parse(Console.ReadLine()); //setting variable for user's cash amount
-            PmtCheck check = new PmtCheck(); //making new cash object with cash class
-
+            Console.WriteLine("Enter account number: ");
+            long acctNum = long.Parse(Console.ReadLine());
+            Console.WriteLine("Enter routing number: ");
+            long routNum = long.Parse(Console.ReadLine());
+            Console.WriteLine("Enter check number: ");
+            int checkNum = int.Parse(Console.ReadLine());
+            PmtCheck check = new PmtCheck(checkAmount, acctNum, routNum, checkNum); //making new cash object with cash class
         }
-
         private static void PaymentCreditCard()
         {
             //getting information from constructor to save CC Info
             Console.WriteLine("Enter total payment due: ");
             double creditCard = double.Parse(Console.ReadLine()); //setting variable for user's cash amount
-            Console.WriteLine("Please enter credit card number: ");
-            double creditCardNumber = double.Parse(Console.ReadLine());
-            Console.WriteLine("Please enter CVV code: ");
+            Console.WriteLine("Enter credit card number: ");
+            long creditCardNumber = long.Parse(Console.ReadLine());
+            Console.WriteLine("Enter CVV code: ");
             int cvv = int.Parse(Console.ReadLine());
-
+            Console.WriteLine("Enter expiration date: mm/yy");
+            DateTime expDate = DateTime.Parse(Console.ReadLine());
             //datetime
             Console.WriteLine("Please input your first and last name: ");
             string userName = Console.ReadLine();
 
-            PmtCreditCard credit = new PmtCreditCard(creditCard, creditCardNumber, cvv, userName); //making new cash object with cash class
+            PmtCreditCard credit = new PmtCreditCard(creditCard, creditCardNumber, cvv, expDate, userName); //making new cash object with cash class
         }
     }
 }
